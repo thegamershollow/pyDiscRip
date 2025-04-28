27e92fd0716ddb2d8bce6fe5cfd0041bf9800045
Shelby Jueden <shelby@jueden.org>
2025-04-28 07:49:20 -0700

    gw options can be set by config

--- handler/media/floppy.py
+++ handler/media/floppy.py
@@ -5,6 +5,7 @@ import subprocess
 import json
 from pathlib import Path
 import importlib
+from pprint import pprint
 
 from handler.media.media_handler import MediaHandler, Media
 from handler.data.data_handler import Data
@@ -21,7 +22,16 @@ class MediaHandlerFloppy(MediaHandler):
         super().__init__()
         self.media_id=Media.Floppy
         self.config_data={
-            "flux_output":"raw"
+            "flux_output":"raw",
+            "gw":{
+                "revs": None,
+                "tracks": None,
+                "hard-sectors": None,
+                "seek-retries": None,
+                "pll": None,
+                "densel": None,
+                "reverse": None
+                }
         }
         self.data_outputs=[Data.Flux]
 
@@ -51,7 +61,32 @@ class MediaHandlerFloppy(MediaHandler):
         args.append("read")
         args.append("--drive")
         args.append(media_sample["Drive"])
+
+        pprint(self.config_data)
+        # Add config options
+        if "revs" in self.config_data["gw"] and self.config_data["gw"]["revs"] is not None:
+            args.append("--revs")
+            args.append(str(self.config_data["gw"]["revs"]))
+        if "tracks" in self.config_data["gw"] and self.config_data["gw"]["tracks"] is not None:
+            args.append("--tracks")
+            args.append(str(self.config_data["gw"]["tracks"]))
+        if "seek-retries" in self.config_data["gw"] and self.config_data["gw"]["seek-retries"] is not None:
+            args.append("--seek-retries")
+            args.append(str(self.config_data["gw"]["seek-retries"]))
+        if "pll" in self.config_data["gw"] and self.config_data["gw"]["pll"] is not None:
+            args.append("--pll")
+            args.append(self.config_data["gw"]["pll"])
+        if "densel" in self.config_data["gw"] and self.config_data["gw"]["densel"] is not None:
+            args.append("--densel")
+            args.append(self.config_data["gw"]["densel"])
+        if "hard-sectors" in self.config_data["gw"] and self.config_data["gw"]["hard-sectors"] is not None:
+            args.append("--hard-sectors")
+        if "reverse" in self.config_data["gw"] and self.config_data["gw"]["reverse"] is not None:
+            args.append("--reverse")
+
         args.append(f"{data["data_dir"]}/{data["data_files"]["flux"]}")
+
+        self.log("floppy_gw_args",args,json_output=True)
 
         res = main(args)
 
